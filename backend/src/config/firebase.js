7094e0217d3eacdc0cee5f7ab6a92e3f74bd476f
Siddiqua <siddiquaabedeen@gmail.com>
2026-07-15 18:36:25 +0530

    Update firebase for Deployment

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -2,8 +2,7 @@ const { initializeApp, cert } = require('firebase-admin/app');
 const { getAuth } = require('firebase-admin/auth');
 const { getMessaging } = require('firebase-admin/messaging');
 
-const serviceAccount = require('./firebase-service-account.json');
-
+const serviceAccount = require('../../firebase-service-account.json');
 const firebaseApp = initializeApp({
   credential: cert(serviceAccount),
 });
